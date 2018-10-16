0e2c37e5588f5198644eeec96105f960cfda3add
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2018-10-16 14:29:28 +0900

    Update scripts/build_lua5.1.sh

--- scripts/build_lua5.1.sh
+++ scripts/build_lua5.1.sh
@@ -14,18 +14,7 @@ cmake .. -DLUA_VERSION=5.1
 make
 
 cd ../..
-cp struct/build/struct.so $build_path/
-
-
-wget https://github.com/renatomaia/luasocket/archive/v2.0.3.tar.gz
-tar xf v2.0.3.tar.gz
-cd luasocket-2.0.3
-wget https://raw.githubusercontent.com/Nobu19800/RTM-Lua/master/thirdparty/luasocket-2.0.3/CMakeLists.txt
-mkdir build
-cd build
-cmake .. -DLUA_VERSION=5.1
-make
-
-cd ../..
 mkdir $build_path/socket
 cp luasocket-2.0.3/build/lib/socket/core.so $build_path/socket
+mkdir $build_path/socket
+cp luasocket-2.0.3/build/lib/oil/bit.so $build_path/oil
